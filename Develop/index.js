d3c5a3a3931f3d00c04ff075e837a72479394daa
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-02-19 11:46:24 -0500

    refactored questions and ran node index to make sure file is generated

--- Develop/index.js
+++ Develop/index.js
@@ -61,42 +61,90 @@ const questions =
             }
         },
         {
-            type: "input",
-            message: "What does the user need to install to run this app?",
-            name: "Installation" 
+            type: 'input',
+            name: 'description',
+            message: "Enter your project description here:",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('It is essential to provide a description of your project.');
+                    return false;
+                }
+            }
         },
         {
-            type: "input",
-            message: "How is this app used? Give instructions.",
-            name: "Usage"
+            type: 'input',
+            name: 'installation',
+            message: 'What are the instructions for installation?',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions for installation to ensure users have the proper software to run the program!');
+                    return false;
+                }
+            }
         },
         {
-            type: "input",
-            message: "What license is being use?",
-            name: "License"
+            type: 'input',
+            name: 'usage',
+            message: 'Instructions for usage:',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Providing instructions for usage will help users properly navigate your project. Please try again.');
+                    return false;
+                }
+            }
         },
         {
-            type:"input",
-            message: "Who contributed to this project?",
-            name: "Contributing"
+            type: 'input',
+            name: 'contributing',
+            message: 'How can others contribute to this project?',
+            validate: contributingInput => {
+                if (contributingInput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions on how others can contribute to your project.');
+                }
+            }
         },
         {
-            type: "input",
-            message: "What commands are need to test this app?",
-            name: "Tests"
+            type: 'input',
+            name: 'tests',
+            message: 'Describe the tests written for your application and how to use them:',
+            validate: testsInput => {
+                if (testsInput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions on how others can contribute to your project.');
+                    return false;
+                }
+            }
         },
         {
-            type: "input",
-            message: "Contact infor inquiries",
-            name: "Questions"
+            type: 'input',
+            name: 'confirmLicenses',
+            message: 'Would you like to include a license?',
+            default: false
         },
         {
-            type: "input",
-            message: "What is your email address?",
-            name: 'Email'
-        }
+            type: 'list',
+            name: 'licenses',
+            message: 'What license would you like to include?',
+            choices: ['MIT', 'GPL', 'CC--0'],
+            when: ({ confirmLicenses }) => {
+                if (confirmLicenses) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
 
-    ]
+    ];
 
 // TODO: Create a function to write README file
 const writeToFile = data => {
